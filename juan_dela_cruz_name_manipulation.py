087273bfbcb23d96c058702994ffc4cb8fa80530
Acee Zai Mendez <zaiakari7@gmail.com>
2026-03-08 01:34:23 +0800

    Added input function for numbers, tried to figure out 000 function using print, however it's an error.

--- juan_dela_cruz_name_manipulation.py
+++ juan_dela_cruz_name_manipulation.py
@@ -1,6 +1,14 @@
+#Program #1
 #Ask for name with leading spaces. ex; ____ Acee Zai Mendez
 full_name = input("Enter your fullname: ")
 #Use lstrip.(), this function removes spaces in the beginning of a string
 clean_name = full_name.lstrip()
 #Print clean_name
 print(clean_name)
+
+#Program #2
+#Ask for numbers 0-1000
+num_input = int(input("Enter a number (0-1000): "))
+# Add zeros to make it a 6 digit format
+print ("000", + num_input)
+
