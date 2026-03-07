bbd3d7fbe80016a3e4df692f6a642b25105b7e81
Acee Zai Mendez <zaiakari7@gmail.com>
2026-03-08 01:51:23 +0800

    Added program 2 & 3. Used upper() function to make all characters uppercase letters.

--- juan_dela_cruz_name_manipulation.py
+++ juan_dela_cruz_name_manipulation.py
@@ -1,6 +1,6 @@
 #Program #1
 #Ask for name with leading spaces. ex; ____ Acee Zai Mendez
-full_name = input("Enter your fullname: ")
+full_name = input("Enter your full name: ")
 #Use lstrip.(), this function removes spaces in the beginning of a string
 clean_name = full_name.lstrip()
 #Print clean_name
@@ -9,6 +9,14 @@ print(clean_name)
 #Program #2
 #Ask for numbers 0-1000
 num_input = int(input("Enter a number (0-1000): "))
-# Add zeros to make it a 6 digit format
-print ("000", + num_input)
+num_sixdigit = f"{num_input:06}"
+print(num_sixdigit)
+
+#Program #3
+#Input mixed lower and uppercase names
+full_name = input("Please enter your full name: ")
+#Use upper() function
+uppercase_name = full_name.upper()
+print(uppercase_name)
+
 
