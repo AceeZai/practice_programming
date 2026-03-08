b13ccd347413e9adcdccfd7d75997477b011191c
Acee Zai Mendez <zaiakari7@gmail.com>
2026-03-08 19:20:59 +0800

    used float for all programs.

--- practicing_printing_strings.py
+++ practicing_printing_strings.py
@@ -4,8 +4,18 @@ second_number = float(input("Enter second number: "))
 print(min(first_number, second_number))
 
 # Prog02: Inequality Check
--
+first_number = float(input("Enter first number: "))
+second_number = float(input("Enter second number: "))
+if first_number != second_number:
+    print("Not Equal")
+
 # Prog03: Simple Subtraction
 first_number = float(input("Enter first number: "))
 second_number = float(input("Enter second number: "))
 print(first_number - second_number)
+
+# Prog04: Floor Division
+dividend = int(input("Enter first number: "))
+divisor = int(input("Enter second number: "))
+print(dividend // divisor)
+
