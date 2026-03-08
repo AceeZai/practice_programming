05f4c430e401c261347122f6d2d34bdeb34537e9
Acee Zai Mendez <zaiakari7@gmail.com>
2026-03-08 20:13:22 +0800

    Used append() function to make program 2 work.

--- manipulate_text_arrangements.py
+++ manipulate_text_arrangements.py
@@ -1,4 +1,13 @@
+#,Prog01: Display Only Non-Duplicates
 user_numbers = [float(input(f"Number {index + 1}: "))
 for current_number in user_numbers:
     if user_numbers.count(current_number) == 1:
     	print(current_numbers)
+    
+#Prog02: Unique Entries Only (First Occurrences)
+user_numbers = [float(input(f"Number {index + 1}: ")) for index in range(10)]
+seen_numbers = []
+for current_number in user_numbers:
+    if current_number not in seen_numbers:
+        print(current_number)
+        seen_numbers.append(current_number)
